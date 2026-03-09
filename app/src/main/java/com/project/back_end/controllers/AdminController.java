2fbfc593462e7ee31ee91393abe8a0f985dffd4f
doberman49 <70598320+doberman49@users.noreply.github.com>
2026-03-09 10:22:51 -0600

    Update AdminController.java

--- app/src/main/java/com/project/back_end/controllers/AdminController.java
+++ app/src/main/java/com/project/back_end/controllers/AdminController.java
@@ -1,7 +1,34 @@
 
 package com.project.back_end.controllers;
 
+import com.project.back_end.models.Admin;
+import com.project.back_end.services.Service;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController // 1. Mark as REST controller
+@RequestMapping("${api.path}admin") // Base URL: /api/admin if api.path=/api/
 public class AdminController {
+
+    private final Service service;
+
+    // 2. Constructor-based injection
+    //@Autowired
+    public AdminController(Service service) {
+        this.service = service;
+    }
+
+    // 3. Admin login endpoint
+    @PostMapping("/login")
+    public ResponseEntity<?> adminLogin(@RequestBody Admin admin) {
+        return service.validateAdmin(admin.getUsername(), admin.getPassword());
+    }
+}
+
+
+
+//public class AdminController {
 
 // 1. Set Up the Controller Class:
 //    - Annotate the class with `@RestController` to indicate that it's a REST controller, used to handle web requests and return JSON responses.
@@ -23,5 +50,5 @@ public class AdminController {
 
 
 
-}
+//}
 
