a015aaa088f264b7eeb03ca027bd8a82ca6cc7fa
doberman49 <70598320+doberman49@users.noreply.github.com>
2026-03-12 16:14:13 -0600

    Update Login.java

--- app/src/main/java/com/project/back_end/DTO/Login.java
+++ app/src/main/java/com/project/back_end/DTO/Login.java
@@ -2,33 +2,16 @@ package com.project.back_end.DTO;
 
 public class Login {
 
-    // 1. Email field for login
-    private String email;
-
-    // 2. Password field for login
+    private String identifier;
     private String password;
 
-    // 3. Default constructor (implicit, but included here for clarity)
-    public Login() {
-    }
+    public Login() {}
 
-    // 4. Getters and Setters
+    public String getIdentifier() { return identifier; }
+    public void setIdentifier(String identifier) { this.identifier = identifier; }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 }
 //public class Login {
     
