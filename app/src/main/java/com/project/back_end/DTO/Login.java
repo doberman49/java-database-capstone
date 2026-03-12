ad4a78a93dd3afb5311cf11e40b7dcf5e0c1b7f0
doberman49 <70598320+doberman49@users.noreply.github.com>
2026-03-12 16:49:29 -0600

    Update Login.java

--- app/src/main/java/com/project/back_end/DTO/Login.java
+++ app/src/main/java/com/project/back_end/DTO/Login.java
@@ -1,17 +1,27 @@
 package com.project.back_end.DTO;
 
 public class Login {
-
     private String identifier;
     private String password;
 
-    public Login() {}
+    public Login() {
+    }
 
-    public String getIdentifier() { return identifier; }
-    public void setIdentifier(String identifier) { this.identifier = identifier; }
+    public String getIdentifier() {
+        return identifier;
+    }
+    
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier; 
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
 //public class Login {
     
