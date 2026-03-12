abd13409c2f76eb2521ccd77f4aca93289c60b04
doberman49 <70598320+doberman49@users.noreply.github.com>
2026-03-12 16:26:17 -0600

    Update AdminController.java

--- app/src/main/java/com/project/back_end/controllers/AdminController.java
+++ app/src/main/java/com/project/back_end/controllers/AdminController.java
@@ -1,46 +1,27 @@
 package com.project.back_end.controllers;
 
-import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.project.back_end.models.Admin;
 import com.project.back_end.services.Service;
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@RestController // 1. Mark as REST controller
-@RequestMapping("${api.path}" + "admin")
+import java.util.Map;
+
+@RestController
+@RequestMapping("${api.path}admin")
 public class AdminController {
+
     private final Service service;
-    // 2. Constructor-based injection
-    @Autowired
+
     public AdminController(Service service) {
         this.service = service;
     }
 
-    // 3. Admin login endpoint
-    //@PostMapping("/login")
-    //public ResponseEntity<?> adminLogin(@RequestBody Admin admin) {
-    //    return service.validateAdmin(admin.getUsername(), admin.getPassword());
-    //}
     @PostMapping
     public ResponseEntity<Map<String, String>> adminLogin(@RequestBody Admin admin) {
         return service.validateAdmin(admin);
     }
-
-    @GetMapping("/dashboard/{token}")
-    public String adminDashboard(@PathVariable String token) {
-        Map<String, String> map=service.validateToken(token,"admin").getBody();
-        if(map==null)
-        {
-            return "admin/adminDashboard";
-        }
-        return "redirect:http://localhost:8080/";    
-    }
 }
-
-
-
 //public class AdminController {
 
 // 1. Set Up the Controller Class:
