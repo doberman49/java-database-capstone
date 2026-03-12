1e37675a362f2d893509d93cc58ff2e1df2d1b02
doberman49 <70598320+doberman49@users.noreply.github.com>
2026-03-12 17:12:11 -0600

    Update DoctorRepository.java

--- app/src/main/java/com/project/back_end/repo/DoctorRepository.java
+++ app/src/main/java/com/project/back_end/repo/DoctorRepository.java
@@ -4,20 +4,15 @@ import com.project.back_end.models.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-
     Doctor findByEmail(String email);
-
     @Query("SELECT d FROM Doctor d WHERE LOWER(d.name) LIKE LOWER(CONCAT('%', :name, '%'))")
     List<Doctor> findByNameLike(String name);
-
     @Query("SELECT d FROM Doctor d WHERE LOWER(d.name) LIKE LOWER(CONCAT('%', :name, '%')) AND LOWER(d.specialty) = LOWER(:specialty)")
     List<Doctor> findByNameContainingIgnoreCaseAndSpecialtyIgnoreCase(String name, String specialty);
-
     List<Doctor> findBySpecialtyIgnoreCase(String specialty);
 }
 
